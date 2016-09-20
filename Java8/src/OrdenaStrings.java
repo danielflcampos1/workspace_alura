179ddf1105968957d1328a05faec189e2f3c9843
Daniel Campos <danielflcampos1@gmail.com>
2016-09-20 09:21:55 -0300

    Working with lambda and method of reference.

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -1,32 +1,43 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
-	
+
 	public static void main(String[] args) {
-		List<String> palavras = new ArrayList<String>();
+
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("casa do código");
+		palavras.add("caelum");
+
+		// Collections.sort(palavras, comparable);
+		palavras.sort((s1, s2) -> {
+			if (s1.length() > s2.length())
+				return -1;
+			if (s1.length() < s2.length())
+				return 1; 
+			return 0;
+		});
+
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String::length));
 		
-		palavras.add("Daniel Campos");
-		palavras.add("Vanessa Soares");
-		palavras.add("Lucia Costa");
-		
-		
-		Comparator<String> comparable = new ComparaPalavras();
-		Collections.sort(palavras, comparable);
+		Function<String , Integer> funcao = s -> s.length();
+		palavras.sort(Comparator.comparing(funcao));
+
 		System.out.println(palavras);
 		
+		palavras.forEach(s -> System.out.println(s));
+		palavras.forEach(System.out::println);
+		
+		new Thread(() -> System.out.println("Executando um Runnable")).start();
+	
 	}
-}
-
-class ComparaPalavras implements Comparator<String>{
-	@Override
-	public int compare(String s1, String s2) {
-		if(s1.length() > s2.length())
-			return -1;
-		if(s1.length() < s2.length())
-			return 1;
-		return 0;
-	}
+	
+	
+	
 }
